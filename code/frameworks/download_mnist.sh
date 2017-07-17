93bcfe2e54d3f23666746f325e1b090e9af04135
Catalina Vajiac <cvajiac@nd.edu>
2017-07-17 13:26:10 -0700

    fix dataset downloading files

--- code/frameworks/download_mnist.sh
+++ code/frameworks/download_mnist.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-mkdir -p /media/datasets/mnist
+DATA_DIR="data/mnist/"
 
-if ! [ -e /media/datasets/mnist/train-images-idx3-ubyte.gz ]
-	then
-		wget -P /media/datasets/mnist/ http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-fi
-gzip -d /media/datasets/mnist/train-images-idx3-ubyte.gz
+DATASETS=("train-images-idx3-ubyte.gz" "train-labels-idx1-ubyte.gz" \
+          "t10k-images-idx3-ubyte.gz" "t10k-labels-idx1-ubyte.gz")
 
-if ! [ -e /media/datasets/mnist/train-labels-idx1-ubyte.gz ]
-	then
-		wget -P /media/datasets/mnist/ http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-fi
-gzip -d /media/datasets/mnist/train-labels-idx1-ubyte.gz
+for DATA in "${DATASETS[@]}"; do
+  if ! [ -e $DATA_DIR$DATA ]; then
+    echo downloading $DATA...
+    wget -P $DATA_DIR "http://yann.lecun.com/exdb/mnist/"$DATA
+    gunzip $DATA_DIR$DATA
+  else
+    echo already have $DATA...
+  fi
+done
 
-if ! [ -e /media/datasets/mnist/t10k-images-idx3-ubyte.gz ]
-	then
-		wget -P /media/datasets/mnist/ http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-fi
-gzip -d /media/datasets/mnist/t10k-images-idx3-ubyte.gz
-
-if ! [ -e /media/datasets/mnist/t10k-labels-idx1-ubyte.gz ]
-	then
-		wget -P /media/datasets/mnist/ http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
-fi
-gzip -d /media/datasets/mnist/t10k-labels-idx1-ubyte.gz
-
+echo done!
