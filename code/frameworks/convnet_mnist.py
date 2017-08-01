ba2fed7a90ec9c2ce391ae8b2f76ffd457ee041a
zhykoties <zhykoties@gmail.com>
2017-08-01 16:19:43 -0700

    Update convnet_mnist.py

--- code/frameworks/convnet_mnist.py
+++ code/frameworks/convnet_mnist.py
@@ -1,29 +1,17 @@
+#!/usr/bin/python
+
 from load_mnist import mnist
-
 from network.cnn_train import iterate_train
-
 import numpy as np
 
 def main():
   trX, teX, trY, teY = mnist(onehot=True)
-
   trX = trX.reshape(-1, 1, 28, 28)
   teX = teX.reshape(-1, 1, 28, 28)
-  perturbation=0.001
 
-  print trY[4]
-
-  for bitsize in [16, 18]:
+  for bitsize in [12, 13, 14, 15, 16, 18, 20, 32]:
     for perturbation in [0.002]:
-      iterate_train(trX, teX, trY, teY, 32, "mnist_FL{}_PERT{}".format(bitsize, perturbation), perturbation)
-  #for bitsize in [12, 13, 14, 15, 16, 18, 20]:
-  #  for perturbation in np.arange(0, 0.01, 0.002):
-  #    iterate_train(trX, teX, trY, teY, 32, "mnist_FL{}_PERT{}".format(bitsize, perturbation), perturbation)
-
-  for bitsize in [32]:
-    for perturbation in np.arange(0.006, 0.01, 0.002):
-      iterate_train(trX, teX, trY, teY, 32, "mnist_FL{}_PERT{}".format(bitsize, perturbation), perturbation)
-      
+      iterate_train(trX, teX, trY, teY, 32, "mnist_FL{}".format(bitsize), perturbation)
   
 if __name__ == "__main__":
   main()
